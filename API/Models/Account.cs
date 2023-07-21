f886ee5f90bf8e440d5d1e424f0742442c326fc1
Shandika Galih <shandika2001@gmail.com>
2023-07-21 16:34:45 +0700

    add modal

---
+++ API/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_accounts")]
+public class Account
+{
+    [Column("password", TypeName = "nvarchar(255)")]
+    public string Password { get; set; }
+}
---
+++ API/Models/AccountRole.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace API.Models;
+
+[Table("tb_tr_account_roles")]
+public class AccountRole
+{
+    [Column("account_guid")]
+    public Guid AccountGuid { get; set; }
+
+    [Column("role_guid")]
+    public Guid RoleGuid { get; set; }
+
+    //Cardinality
+    public Role? Role { get; set; }
+    public Account? Account { get; set; }
+}
