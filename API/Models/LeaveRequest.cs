1e9bb1f5c7182144b1efd8d72ccf1cea2cd40266
gilanga-akram <71384407+gilanga-akram@users.noreply.github.com>
2023-07-21 16:58:11 +0700

    Leave Request & Leave Types

--- API/Models/LeaveRequest.cs
+++ API/Models/LeaveRequest.cs
@@ -1,6 +1,36 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
 {
+    [Table("tr_m_leave_request")]
     public class LeaveRequest
     {
+        [Column("status")]
+        public Enum Status { get; set; }
+       
+        [Column("start_date")]
+        public DateOnly Startdate  { get; set; }
+
+        [Column("end_date")]
+        public DateOnly Enddate { get; set; }
+
+        [Column("submit_date")]
+        public DateTime SubmitDate { get; set; }
+
+        [Column("remarks", TypeName = "varchar(max)")]
+        public string Remarks { get; set; }
+
+
+        [Column("eligible_leave")]
+        public int EligibleLeave {get; set; }
+
+        [Column("total_leave")]
+        public int TotalLeave { get; set; }
+
+        [Column("leave_types")]
+        public Guid LeaveTypes { get; set; }
+
+        [Column("Employees")]
+        public Guid Employees { get; set; }
     }
 }
--- API/Models/LeaveTypes.cs
+++ API/Models/LeaveTypes.cs
@@ -1,6 +1,14 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
 {
+    [Table("tr_m_leave_types")]
     public class LeaveTypes
     {
+        [Column("leave_name", TypeName = "nvarchar(50)")]
+        public string LeaveName { get; set; }
+
+        [Column("leave_description", TypeName = "varchar(max)")]
+        public string LeaveDescription { get; set;}
     }
 }
