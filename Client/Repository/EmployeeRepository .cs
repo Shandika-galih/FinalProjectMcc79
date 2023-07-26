4d4728487001f260bd151eda79effd66589158ef
Shandika Galih <shandika2001@gmail.com>
2023-07-26 14:48:47 +0700

    new

---
+++ Client/Repository/EmployeeRepository .cs
@@ -0,0 +1,13 @@
+﻿using API.DTOs.Employees;
+using Client.Contract;
+
+namespace Client.Repository;
+
+public class EmployeeRepository : GeneralRepository<GetDataEmployeeDto, Guid>, IEmployeeRepository
+{
+    public EmployeeRepository(string request = "employees/") : base(request)
+    {
+
+        
+    }
+}
