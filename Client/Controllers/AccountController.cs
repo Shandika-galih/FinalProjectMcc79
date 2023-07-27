1eb60d2804d30b6b141c311f03782cbecf596cfb
gilanga-akram <71384407+gilanga-akram@users.noreply.github.com>
2023-07-27 13:55:37 +0700

    fitur login

---
+++ Client/Controllers/AccountController.cs
@@ -0,0 +1,88 @@
+﻿using API.DTOs.Accounts;
+using Microsoft.AspNetCore.Mvc;
+using Client.Contract;
+using Client.ViewModels.Account;
+
+namespace Client.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginVM register)
+        {
+
+            var result = await _accountRepository.Register(register);
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            else if (result.Status == "BadRequest")
+            {
+                ModelState.AddModelError(string.Empty, result.Message);
+                TempData["Error"] = $"Something Went Wrong! - {result.Message}!";
+                return View();
+            }
+            else if (result.Status == "OK")
+            {
+                TempData["Success"] = $"Data has been Successfully Registered! - {result.Message}!";
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM login)
+        {
+            var result = await _accountRepository.Login(login);
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            else if (result.Status == "BadRequest")
+            {
+                ModelState.AddModelError(string.Empty, result.Message);
+                return View();
+            }
+            else if (result.Status == "OK")
+            {
+                HttpContext.Session.SetString("JWToken", result.Data);
+                return RedirectToAction("Index", "Employee");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Account");
+        }
+    }
+}
---
+++ Client/Repositories/AccountRepository.cs
@@ -0,0 +1,47 @@
+﻿using API.DTOs.Accounts;
+using API.Utilities;
+using Client.Contract;
+using Client.Repository;
+using Client.ViewModels.Account;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Client.Repositories;
+
+public class AccountRepository : GeneralRepository<LoginVM, string>, IAccountRepository
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _request;
+    public AccountRepository(string request = "accounts/") : base(request)
+    {
+        _request = request;
+        _httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri("https://localhost:7114/api/")
+        };
+    }
+
+    public async Task<ResponseHandler<AccountRepository>> Register(LoginVM entity)
+    {
+        ResponseHandler<AccountRepository> entityVM = null;
+        StringContent content = new StringContent(JsonConvert.SerializeObject(entity), Encoding.UTF8, "application/json");
+        using (var response = _httpClient.PostAsync(_request + "register", content).Result)
+        {
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            entityVM = JsonConvert.DeserializeObject<ResponseHandler<AccountRepository>>(apiResponse);
+        }
+        return entityVM;
+    }
+
+    public async Task<ResponseHandler<string>> Login(LoginVM login)
+    {
+        ResponseHandler<string> responseHandler = null;
+        StringContent content = new StringContent(JsonConvert.SerializeObject(login), Encoding.UTF8, "application/json");
+        using (var response = await _httpClient.PostAsync(_request + "login", content))
+        {
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            responseHandler = JsonConvert.DeserializeObject<ResponseHandler<string>>(apiResponse);
+        }
+        return responseHandler;
+    }
+}
