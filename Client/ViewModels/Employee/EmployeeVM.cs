d4011bccbb7ff0f450462c21bc0d8c4886b22207
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-07-27 21:55:05 +0700

    feat: create employee feature

--- Client/ViewModels/Employee/EmployeeVM.cs
+++ Client/ViewModels/Employee/EmployeeVM.cs
@@ -6,9 +6,9 @@ public class EmployeeVM
 {
     public Guid Guid { get; set; }
     public int NIK { get; set; }
-    public string FullName { get; set; } 
+    public string? FullName { get; set; } 
     public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? LastName { get; set; }
     public GenderEnum Gender { get; set; }
     public string PhoneNumber { get; set; }
     public string Email { get; set; }
@@ -17,4 +17,6 @@ public class EmployeeVM
     public Guid? ManagerGuid { get; set; }
     public string Manager { get; set; }
     public string RoleName { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
 }
